3519cf0f1f0d92757cd533b756293b45b2cc4be2
Ore <orellanojoaquinmarcelo@gmail.com>
2023-04-21 10:22:11 -0300

    Guia 3 ejercicio 2 listo

---
+++ Ejercicios/guia-3/ejer2.py
@@ -0,0 +1,10 @@
+try:
+    num1 = float(input("Ingresar un numero: "))
+    num2 = float    (input("Ingresar un numero: "))
+    print("El resultado es " + str(num1+num2))
+except ZeroDivisionError:
+    print("NO PUEDES DIVIDIR POR CERO")
+
+if "y" == input().upper(): ejecutar()
+
+
