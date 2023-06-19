8283d88d6c27ca40cdbcc5460c46f38b3965d9aa
Ore <orellanojoaquinmarcelo@gmail.com>
2023-06-19 20:53:28 -0300

    correccion de ejer4 guia 4

--- Ejercicios/guia-4/ejer4/ejer4.py
+++ Ejercicios/guia-4/ejer4/ejer4.py
@@ -7,6 +7,3 @@ def calculate_statistics(numbers: list) -> tuple[float, float]:
     desviacion_estandar = (suma_diferencias_cuadrado / n) ** 0.5
 
     return media, desviacion_estandar
-
-
-print(calculate_statistics([2, 4, 6, 8, 10]))
